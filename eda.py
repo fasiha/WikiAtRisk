1aa75f04083833e2600301bfa24ee9dd4c660bfb
Ahmed Fasih <wuzzyview@gmail.com>
2018-04-05 23:31:06 -0400

    ACF and FFT of ACF is very helpful

--- eda.py
+++ eda.py
@@ -12,11 +12,17 @@ with open('wikilangs.json', 'r') as f:
 langToData = {o['prefix']: o for o in wikilangs}
 
 langs = 'en,fr,ja,ru,zh,ar,he'.split(',')
-ds = xr.merge([xr.open_dataset('latest/editors__{}.wikipedia.nc'.format(lang)) for lang in langs])
+endpoint = 'bytes-difference_net'
+endpoint = 'editors'
+ds = xr.merge(
+    [xr.open_dataset('latest/{}__{}.wikipedia.nc'.format(endpoint, lang)) for lang in langs])
 
-edits = ds.loc[dict(
-    pageType='content', activityLevel='all-activity-levels', editorType=['anonymous',
-                                                                         'user'])].sum('editorType')
+subdict = dict(
+    pageType='content', activityLevel='all-activity-levels', editorType=['anonymous', 'user'])
+for key in list(subdict.keys()):
+    if key not in ds.coords:
+        del subdict[key]
+edits = ds.loc[subdict].sum('editorType')
 edits = xr.concat(edits.data_vars.values(), 'lang')
 edits.coords['lang'] = langs
 
@@ -27,8 +33,8 @@ def dow(lang, edits):
     [plt.plot(edits.coords['time'][start::7], edits.loc[lang][start::7]) for start in range(7)]
     plt.legend(dayOfWeek[:7].values)
     plt.xlabel('date')
-    plt.ylabel('editors')
-    plt.title('Daily active {} Wikipedia editors'.format(langToData[lang]['lang']))
+    plt.ylabel(endpoint)
+    plt.title('Daily active {} Wikipedia {}'.format(langToData[lang]['lang'], endpoint))
     [plt.savefig('{}-dayofweek.{}'.format(lang, f)) for f in 'svg,png'.split(',')]
 
 
@@ -39,8 +45,8 @@ plt.figure()
 plt.semilogy(edits.coords['time'][:-1], edits.values.T[:-1, :])
 plt.legend([langToData[lang]['lang'] for lang in langs])
 plt.xlabel('date')
-plt.ylabel('editors')
-plt.title('Daily active Wikipedia editors')
+plt.ylabel(endpoint)
+plt.title('Daily active Wikipedia {}'.format(endpoint))
 plt.savefig('editors.png')
 plt.savefig('editors.svg')
 
@@ -140,3 +146,71 @@ plt.figure()
 m = plt.pcolormesh(2001 + t, f, en, vmax=0, vmin=-60)
 plt.xlabel('year')
 plt.ylabel('cycles/year')
+
+from scipy.signal import correlate
+
+
+def acorrBiased(y):
+    """Obtain the biased autocorrelation and its lags
+  """
+    r = correlate(y, y) / len(y)
+    l = np.arange(-(len(y) - 1), len(y))
+    return r, l
+
+
+def acorrc(y, maxlags=None):
+    maxlags = maxlags or (len(y) - 1)
+    a = [1] + [np.corrcoef([y[n:], y[:-n]])[0, 1] for n in range(1, maxlags)]
+    return a, range(len(a))
+
+
+def acorrc2(arr, maxlags=None):
+    a = np.vstack([acorrc(y, maxlags=maxlags)[0] for y in arr])
+    return a, range(a.shape[1])
+
+
+ac, lags = acorrc2(edits.values[:, spectralStart:-1])
+plt.figure()
+plt.plot(lags, ac.T)
+plt.legend([langToData[lang]['lang'] for lang in langs])
+plt.xlabel('lag (days)')
+plt.ylabel('correlation')
+plt.title('Auto-correlation btw days(-1 to +1), {}'.format(endpoint))
+
+import numpy.fft as fft
+from scipy.signal import detrend, hamming
+
+acf = np.abs(fft.rfft(hamming(ac.shape[1]) * ac, n=1024 * 16, axis=-1))
+fac = fft.rfftfreq(16 * 1024, d=1 / 365)
+plt.figure()
+plt.loglog(365 / fac, acf.T)
+plt.legend([langToData[lang]['lang'] for lang in langs])
+plt.title('Spectrum of ACF')
+plt.xlabel('period (days)')
+
+lidx = 1
+en = edits.values[lidx, spectralStart:-1]
+lag = 550
+lag = 2712
+lag = 184
+plt.figure()
+h, xe, ye, i = plt.hist2d(en[:-lag], en[lag:], 50)
+plt.xlabel('{} now'.format(endpoint))
+plt.ylabel('{} after {} days'.format(endpoint, lag))
+plt.title('{}'.format(langs[lidx]))
+plt.plot([0, 45e3], [0, 45e3], 'k:')
+plt.figure()
+plt.scatter(en[:-lag], en[lag:])
+
+
+def cepstrum(y, fs=1):
+    y = detrend(y, type='linear')
+    nfft = 16 * 1024
+    f = fft.fftfreq(nfft, d=1 / fs)
+    f = fft.fftfreq(nfft, d=f[1] - f[0])
+    return (fft.fftshift(np.abs(fft.ifft(np.log(np.abs(fft.fft(y, nfft)))))), fft.fftshift(f))
+
+
+c, f = cepstrum(en, 365.)
+plt.figure()
+plt.semilogy(f, c)
